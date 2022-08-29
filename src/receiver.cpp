33a6ba75b173ab8e9c037c4e48f730111b6f619f
Samuel Valis <samuel.valis@meluzina.sk>
2022-08-29 18:24:15 +0200

    comments

--- src/receiver.cpp
+++ src/receiver.cpp
@@ -10,8 +10,18 @@ unsigned long Receiver::readValue() {
 
     DEBUG_PRINT("Receiver value = ");
     DEBUG_PRINTLN(duration);
+
+    // old (dig):
     // Receiver value = 1224
     // Receiver value = 1836
+    // Receiver value = 1500 1501 tx turned off
+
+    // new:
+    // Receiver value = 1500 1501 tx turned off
+    // Receiver value = 0 tx turned off
+    // 1500 - no press
+    // 1836 - up
+    // 1249 - down
 
     return duration;
 }
