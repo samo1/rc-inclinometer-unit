ba75deeded1c42b528dcedb1dad830bdc186e5e7
Samuel Valis <samuel.valis@meluzina.sk>
2022-11-18 18:48:21 +0100

    comments

--- include/receiver.h
+++ include/receiver.h
@@ -3,8 +3,13 @@
 
 #include "debug.h"
 
+// command to be executed, based on the physical switch position and the internal state
 enum class ReceiverCommand { none, winchStop, winchIn, winchOut };
+
+// internal state machine
 enum class ReceiverState { winchStopped, winchingIn, winchingOut };
+
+// current position of the physical switch on the controller
 enum class SwitchPosition { off, up, down };
 
 class Receiver {
