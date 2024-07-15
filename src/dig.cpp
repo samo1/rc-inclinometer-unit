9ea51b51fef771afc7218df9deb39a7e93749848
Samuel Valis <samuel.valis@meluzina.sk>
2024-07-15 19:33:08 +0200

    changed dig endpoints for normal size servo

--- src/dig.cpp
+++ src/dig.cpp
@@ -1,10 +1,10 @@
 #include "dig.h"
 #include "debug.h"
 
-#define FRONT_DIG_OFF 55
-#define FRONT_DIG_OFF_2 52
+#define FRONT_DIG_OFF 113
+#define FRONT_DIG_OFF_2 115
 #define FRONT_DIG_OFF_2_DELAY_MS 1000
-#define FRONT_DIG_ON 0
+#define FRONT_DIG_ON 30
 
 void Dig::initialize() {
     DEBUG_PRINTLN("Attaching dig control on D6");
