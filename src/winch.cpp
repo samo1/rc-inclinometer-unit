399211ea5b96a281404dbeaaf2e29e31d22974b7
Samuel Valis <samuel.valis@meluzina.sk>
2025-02-11 18:51:15 +0100

    new winch controller

--- src/winch.cpp
+++ src/winch.cpp
@@ -2,14 +2,22 @@
 #include "debug.h"
 
 void Winch::initialize() {
-    DEBUG_PRINTLN("Attaching winch control on D9");
+    DEBUG_PRINTLN("Attaching winch control");
     servo.attach(D9);
+#ifdef L298N_WINCH_CONTROLLER
+    pinMode(D11, OUTPUT);
+    pinMode(D12, OUTPUT);
+#endif
     stop();
     disable();
 }
 
 void Winch::stop() {
     DEBUG_PRINTLN("Winch stop");
+#ifdef L298N_WINCH_CONTROLLER
+    digitalWrite(D11, LOW);
+    digitalWrite(D12, LOW);
+#endif
     servo.write(0);
     statusInfo.winchStopped();
 }
@@ -17,7 +25,13 @@ void Winch::stop() {
 void Winch::in() {
     DEBUG_PRINTLN("Winch in");
     if (enabled) {
+#ifdef L298N_WINCH_CONTROLLER
+        digitalWrite(D11, LOW);
+        digitalWrite(D12, HIGH);
+        servo.write(100);
+#else
         servo.write(45);
+#endif
         statusInfo.winchIn();
     }
 }
@@ -25,7 +39,13 @@ void Winch::in() {
 void Winch::out() {
     DEBUG_PRINTLN("Winch out");
     if (enabled) {
+#ifdef L298N_WINCH_CONTROLLER
+        digitalWrite(D11, HIGH);
+        digitalWrite(D12, LOW);
+        servo.write(100);
+#else
         servo.write(135);
+#endif
         statusInfo.winchOut();
     }
 }
